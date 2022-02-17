c634d844ad00eaefe80635c3a3835e84c33ecc69
Ahmed Touati <atouati@fb.com>
2022-02-17 19:05:30 +0100

    create linear bandit env

--- envs/linear.py
+++ envs/linear.py
@@ -1,41 +1,46 @@
 import numpy as np
 from dataclasses import dataclass
 from typing import Optional, Tuple
-from .spaces import DiscreteFix
 
 @dataclass
-class Bandit_Linear:
+class LinearBandit:
 
-    feature_dim: int
+    context_dim: int
     noise: float
-    arms: int
-    seed: Optional[int]=0
+    num_actions: int
+    seed: Optional[int] = 0
 
     def __post_init__(self) -> None:
-        self.theta = np.random.uniform(low=-1, high=1, size=(self.feature_dim,))
-        self.np_random = np.random.RandomState(self.seed)
-        self.action_space = DiscreteFix(n=self.arms)
+        self.theta = np.random.uniform(low=-1, high=1, size=(self.num_actions, self.context_dim))
 
-    def sample_context(self) -> np.ndarray:
-        self.phi = self.np_random.uniform(low=-1, high=1, size=(self.arms, self.feature_dim))
-        return self.phi
+    def _sample_context(self) -> np.ndarray:
+        self.context = np.random.uniform(low=-1, high=1, size=(self.context_dim, ))
+        return self.context
 
-    def features(self) -> np.ndarray:
-        return self.phi
+    def sample(self) -> np.ndarray:
+        """ sample a context and return its expanded feature
+        """
+        feature_dim = self.context_dim * self.num_actions
+        context = self._sample_context().copy()
+        feat = np.zeros((self.num_actions, feature_dim))
+        for a in range(self.num_actions):
+            feat[a, a * self.context_dim: a * self.context_dim + self.context_dim] = context
+        self.feat = feat
+        return feat
 
     def step(self, action: int) -> float:
-        reward = self.phi[action].dot(self.theta)
-        noise = self.np_random.randn(1).item() * self.noise
+        reward = np.dot(self.theta[action], self.context)
+        noise = self.noise * np.random.randn()
         return reward + noise
 
     def best_reward_and_action(self) -> Tuple[int, float]:
         """ Best action and reward in the current context
         """
-        rewards = self.phi @ self.theta
+        rewards = np.dot(self.theta, self.context)
         action = np.argmax(rewards)
         return rewards[action], action
     
     def expected_reward(self, action: int) -> float:
-        rewards = self.phi @ self.theta
+        rewards = np.dot(self.theta, self.context)
         return rewards[action]
         
