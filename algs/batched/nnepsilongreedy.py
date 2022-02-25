c672151ffe2f8360a03c5067c16f163f883dec25
Ahmed Touati <atouati@fb.com>
2022-02-25 13:50:46 +0100

    indentation

--- algs/batched/nnepsilongreedy.py
+++ algs/batched/nnepsilongreedy.py
@@ -7,13 +7,22 @@ from torch.nn import functional as F
 
 from .templates import XBModule
 
-class NNEpsGreedy(XBModule):
 
+class NNEpsGreedy(XBModule):
     def __init__(
-        self, env: Any, model: nn.Module, device: Optional[str] = "cpu", batch_size: Optional[int] = 256, max_updates: Optional[int] = 1, learning_rate: Optional[float] = 0.001, weight_decay: Optional[float] = 0, buffer_capacity: Optional[int] = 10000, seed: Optional[int] = 0, reset_model_at_train: Optional[bool] = True, update_every_n_steps: Optional[int] = 100,
-        epsilon_min: float=0.05,
-        epsilon_start: float=2,
-        epsilon_decay: float=200,
+        self, env: Any, model: nn.Module,
+            device: Optional[str] = "cpu",
+            batch_size: Optional[int] = 256,
+            max_updates: Optional[int] = 1,
+            learning_rate: Optional[float] = 0.001,
+            weight_decay: Optional[float] = 0,
+            buffer_capacity: Optional[int] = 10000,
+            seed: Optional[int] = 0,
+            reset_model_at_train: Optional[bool] = True,
+            update_every_n_steps: Optional[int] = 100,
+            epsilon_min: float=0.05,
+            epsilon_start: float=2,
+            epsilon_decay: float=200,
     ) -> None:
         super().__init__(env, model, device, batch_size, max_updates, learning_rate, weight_decay, buffer_capacity, seed, reset_model_at_train, update_every_n_steps)
         self.epsilon_min = epsilon_min
