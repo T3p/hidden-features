398953cc82e6fa89c6e00423412f040a627e8b2c
Andrea <andrea.tirinzoni@gmail.com>
2022-04-12 15:04:01 +0200

    fix bug in exponential schedule for network retraining

--- xbrl/algs/batched/templates.py
+++ xbrl/algs/batched/templates.py
@@ -53,7 +53,10 @@ class XBModule(nn.Module):
         self.batch_counter = 0
         if self.model:
             self.model.to(self.device)
-        self.update_time = 0
+
+        # TODO: check the following lines: with initialization to 0 the training code is never called
+        # self.update_time = 0
+        self.update_time = 2**np.ceil(np.log2(self.batch_size)) + 1
 
     def _post_train(self, loader=None) -> None:
         pass
