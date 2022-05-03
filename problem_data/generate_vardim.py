6833c4676338f1a470330c02938f8b2522af187e
Matteo Pirotta <teopir@users.noreply.github.com>
2022-05-03 13:47:02 +0200

    Modified vardim test to reflect ICML paper

--- problem_data/generate_vardim.py
+++ problem_data/generate_vardim.py
@@ -2,10 +2,12 @@ import numpy as np
 import sys
 sys.path.insert(0, '..')
 from lbrl.linearenv import LinearEnv, LinearRepresentation
-from lbrl.hlsutils import derank_hls, hls_lambda, is_hls, reduce_dim
+from lbrl.hlsutils import derank_hls, hls_lambda, is_hls, reduce_dim, make_reshaped_linrep
 import json
+from lbrl.utils import make_synthetic_features
 
-out_file = "vardimtest_c1.npy"
+# out_file = "vardimtest_icml_nonrealizable.npy"
+out_file = "vardimtest_icml_realizable.npy"
 seed_problem = 99
 
 # in_features_file = "basic_features.npy"
@@ -18,7 +20,7 @@ with open(json_file, 'r') as f:
     data = json.load(f)
     theta = np.array(data['param'])
     features = np.array(data['features'])
-dim = len(theta)
+na, nc, dim = features.shape
 print(f"dim: {dim}")
 true_reward = features @ theta
 problem_gen = np.random.RandomState(seed_problem)
@@ -34,6 +36,23 @@ for i in range(2, dim+1):
     param_list.append(pi)
 
 reference_rep = 0
+if out_file == "vardimtest_icml_nonrealizable.npy":
+    f1, p1 = make_reshaped_linrep(features=features, param=theta, newdim=int(dim/2), transform=True, normalize=True, seed=seed_problem)
+    rep_list.append(f1)
+    param_list.append(p1)
+    f2, p2 = make_reshaped_linrep(features=features, param=theta, newdim=int(dim/3), transform=True, normalize=True, seed=seed_problem)
+    rep_list.append(f2)
+    param_list.append(p2)
+    f3, p3 = make_synthetic_features(n_contexts=nc, n_actions=na, dim=3,
+        context_generation="uniform", feature_expansion="none", seed=seed_problem)
+    rep_list.append(f3)
+    param_list.append(p3)
+    f4, p4 = make_synthetic_features(n_contexts=nc, n_actions=na, dim=9,
+        context_generation="uniform", feature_expansion="none", seed=seed_problem)
+    rep_list.append(f4)
+    param_list.append(p4)
+
+print(f"total rep: {len(rep_list)}")
 
 np.save(out_file, np.array([rep_list, param_list, reference_rep], dtype=object), allow_pickle=True)
 a,b,c = np.load(out_file, allow_pickle=True)
