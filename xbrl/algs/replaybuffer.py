1cd94d215c515ea88de72b3a467f161232b94242
Ahmed Touati <atouati@fb.com>
2022-05-04 10:54:05 +0200

    refactor template

--- xbrl/algs/replaybuffer.py
+++ xbrl/algs/replaybuffer.py
@@ -8,7 +8,7 @@ class SimpleBuffer:
     def __init__(self, capacity: int) -> None:
         self.buffer = deque(maxlen=capacity)
 
-    def __len__(self) -> None:
+    def __len__(self) -> int:
         return len(self.buffer)
 
     def append(self, experience: Tuple) -> None:
@@ -17,9 +17,9 @@ class SimpleBuffer:
         self.buffer.append(experience)
 
     def get_all(self):
-        return self.sample(size=len(self.buffer))
+        return self.sample(batch_size=len(self.buffer))
 
-    def sample(self, size:int):
-        indices = np.random.choice(len(self.buffer), min(size, len(self.buffer)), replace=False)
+    def sample(self, batch_size:int):
+        indices = np.random.choice(len(self.buffer), size=min(batch_size, len(self.buffer)), replace=False)
         out = (np.array(el) for el in zip(*(self.buffer[idx] for idx in indices)))
         return out
