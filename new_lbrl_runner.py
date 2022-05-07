- Added new way of generating bandits problem via categorical distribution - fixed computation of minimum gap (added tolerance)

new_lbrl_runner.py
@@ -134,20 +134,9 @@ def my_app(cfg: DictConfig) -> None:
         del features_list
 
     # compute gap
-    min_gap = np.inf
-    min_gap_ctx = []
-    na = true_reward.shape[1]
-    for ctx in range(true_reward.shape[0]):
-        rr = true_reward[ctx]
-        arr = sorted(rr)
-        for i in range(na-1):
-            diff = arr[i+1] - arr[i]
-            if diff <= min_gap and diff > 0:
-                min_gap = diff
-                min_gap_ctx.append(ctx)
+    min_gap = env.min_suboptimality_gap(tol=1e-6)
 
     log.info(f"min gap: {min_gap}")
-    log.info(f"min gap [rewards]: {true_reward[min_gap_ctx]}")
 
     for i in range(len(rep_list)):
         log.info("\n")

problem_data/generate_largegap.py
@@ -7,18 +7,24 @@ import json
 from lbrl.utils import make_synthetic_features
 
 nc=1000
-na=5
+na=6
 dim=10
 seed_problem=2311
+DECIMALS=2
 
 min_gap = 0 
 count = 0
+EL = np.linspace(-2,2,21).astype(np.float32)
+print(EL)
 while min_gap < 0.1 and count<10000:
     count += 1
     features, param = make_synthetic_features(n_contexts=nc, n_actions=na, dim=dim,
         context_generation="bernoulli", feature_expansion="none", seed=seed_problem)
     # features = np.random.binomial(1,p=0.75,size=(nc,na,dim)).reshape(nc,na,dim)
-    param = np.random.binomial(1,p=0.9,size=dim)
+    features = np.random.choice(EL, size=(nc,na,dim))
+    features = np.round(features, decimals=DECIMALS)
+    # param = np.random.binomial(1,p=0.9,size=dim)
+    param = np.random.choice([-1,0,1], p=[0.4,0.2,0.4], size=dim)
     true_reward=features @ param
 
     # compute gap
@@ -30,10 +36,12 @@ while min_gap < 0.1 and count<10000:
         assert len(arr) == na
         gap = np.inf
         for i in range(na-1):
-            diff = arr[i+1] - arr[i]
-            if diff <= min_gap and diff > 0:
+            diff = arr[na-1] - arr[i]
+            if np.abs(diff-min_gap) < 1e-6:
+                min_gap_ctx = [ctx]
+            elif diff < min_gap and diff > 1e-6:
                 min_gap = diff
-                min_gap_ctx.append(ctx)
+                min_gap_ctx = [ctx]
     
     if  min_gap > 0.1:
         print(f"min_gap: {min_gap}")
@@ -50,13 +58,15 @@ while min_gap < 0.1 and count<10000:
         print(f"is HLS: {is_hls(features, true_reward, tol=1e-6)}")
         print(f"HSL rank: {hls_rank(features, true_reward, tol=1e-6)}")
         print(f"lambda HLS: {hls_lambda(features, true_reward)}")
-        print(true_reward)
+        # print(true_reward)
+
+        print(f"param: {param}")
 
         rep_list = [features]
         param_list = [param]
         position_reference_rep = 0
 
-out_file = "bernoulli_gapmin1.npy"
+out_file = "categorical_largegap.npy"
 np.save(out_file, np.array([rep_list, param_list, position_reference_rep], dtype=object), allow_pickle=True)
 a,b,c = np.load(out_file, allow_pickle=True)
 

xbrl/algs/batched/nnlinucb.py
@@ -62,7 +62,7 @@ class NNLinUCB(XBModule):
             A = torch.matmul(phi.T, phi) + self.ucb_regularizer * torch.eye(phi.shape[1], device=self.device)
             A /= phi.shape[0]
             # det_loss = torch.logdet(A)
-            spectral_loss = - torch.log(torch.linalg.eigvalsh(A).min())
+            spectral_loss = - torch.log(torch.linalg.eigvalsh(A)[0])
             loss = loss + self.weight_spectral * spectral_loss
             metrics['spectral_loss'] = spectral_loss.cpu().item()
 
@@ -197,14 +197,14 @@ class NNLinUCB(XBModule):
                 all_rewards = self.env.rewards.reshape(-1)
                 error, _ = self.compute_linear_error(all_features, all_rewards)
                 max_err = torch.abs(error).max()
-                mean_square_err = error.pow(2).mean()
+                mean_abs_err = torch.abs(error).mean()
                 if self.use_tb:
                     self.writer.add_scalar('max miss-specification', max_err.cpu().item(), self.t)
-                    self.writer.add_scalar('mean square miss-specification', mean_square_err.cpu().item(), self.t)
+                    self.writer.add_scalar('mean abs miss-specification', mean_abs_err.cpu().item(), self.t)
 
                 if self.use_wandb:
                     wandb.log({'max miss-specification': max_err.cpu().item()}, step=self.t)
-                    wandb.log({'mean square miss-specification': mean_square_err.cpu().item()}, step=self.t)
+                    wandb.log({'mean abs miss-specification': mean_abs_err.cpu().item()}, step=self.t)
 
     def compute_linear_error(self, features: np.ndarray, reward: np.ndarray):
         assert len(features.shape) == 2 and len(reward.shape) == 1
@@ -269,13 +269,13 @@ class NNLinUCB(XBModule):
                 self.writer.add_scalar('hls_lambda', hls_lambda, self.t)
                 self.writer.add_scalar('hls_rank', hls_rank, self.t)
                 self.writer.add_scalar('hls?', ishls, self.t)
-                self.writer.add_scalar('rank_phi', rank_phi, self.t)
+                self.writer.add_scalar('total rank', rank_phi, self.t)
             if self.use_wandb:
                 wandb.log({'max miss-specification': max_err.cpu().item(),
                            'hls_lambda': hls_lambda,
                            'hls_rank': hls_rank,
                            'hls?': ishls,
-                           'rank_phi': rank_phi}, step=self.t)
+                           'total rank': rank_phi}, step=self.t)
 
 
     # def _post_train(self, loader=None):

xbrl/envs/cbfinite.py
@@ -72,13 +72,13 @@ class CBFinite:
     def expected_reward(self, action: int) -> float:
         return self.rewards[self.idx, action]
 
-    def min_suboptimality_gap(self):
+    def min_suboptimality_gap(self, tol=1e-6):
         min_gap = np.inf
         for ctx in range(self.__len__()):
             arr = sorted(self.rewards[ctx])
             for i in range(self.n_arms-1):
-                diff = arr[i+1] - arr[i]
-                if diff <= min_gap and diff > 0:
+                diff = arr[self.n_arms-1] - arr[i]
+                if diff < min_gap - tol and diff > tol:
                     min_gap = diff
         return min_gap
 