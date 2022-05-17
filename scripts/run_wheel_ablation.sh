c65eed0249652c1f3dca193d1bb41da384997a3d
Andrea Tirinzoni <tirinzoni@fb.com>
2022-05-17 22:43:40 +0200

    script wheel

--- scripts/run_wheel_ablation.sh
+++ scripts/run_wheel_ablation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 HORIZON=200000
-SEEDS=713,464,777,879,660,608,773,919,591,229,727,184,307,662,600,770,442,150,336,52
+# SEEDS=713,464,777,879,660,608,773,919,591,229,727,184,307,662,600,770,442,150,336,52
+SEEDS=713,464,777,879,660,608,773,919,591,229
 ALGO=nnegreedy
 
 # MSE
@@ -20,3 +21,5 @@ python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=
 python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=${ALGO} check_glrt=True glrt_scale=5 layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=1 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=False domain=wheel.yaml
 # Trace (maxnorm)
 python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=${ALGO} check_glrt=True glrt_scale=5 layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=1 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=True domain=wheel.yaml
+# Trace
+python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=${ALGO} check_glrt=True glrt_scale=5 layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=1 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=False domain=wheel.yaml
