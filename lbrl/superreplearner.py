49e043f4bd5a6e38f403d3826ce5efd396e00d14
Matteo Pirotta <teopir@users.noreply.github.com>
2022-05-18 10:45:20 +0200

    Updated

--- lbrl/superreplearner.py
+++ lbrl/superreplearner.py
@@ -54,10 +54,10 @@ class SuperRepLearner:
         for i in range(M):
             dim = self.reps[i].features_dim()
             self.inv_A.append(np.eye(dim) / self.cfg.reg_val)
-            if self.reg_matrix_rl:
+            if self.cfg.reg_matrix_rl:
                 self.Amtx.append(self.cfg.reg_val * np.eye(dim))
             else:
-                self.Amtx.append(np.zeros(dim))
+                self.Amtx.append(np.zeros((dim,dim)))
             self.b_vec.append(np.zeros(dim))
             self.A_logdet.append(np.log(self.cfg.reg_val) * dim)
             self.theta.append(self.inv_A[i] @ self.b_vec[i])
