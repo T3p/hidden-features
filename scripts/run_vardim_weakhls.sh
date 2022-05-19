9ba4c14f9f91fe0faf2429f6ddcc6fad0ffb46ef
Matteo Pirotta <teopir@users.noreply.github.com>
2022-05-19 08:39:55 +0200

    updated

--- scripts/run_vardim_weakhls.sh
+++ scripts/run_vardim_weakhls.sh
@@ -2,11 +2,39 @@
 
 TODAY=$(date +'%Y-%m-%d')
 HORIZON=80000
-SEEDS=3720889,11982979,8840435,11768055,1522722,6914388,9554994,6882166,11793768,8405117,1693841,2630410,10264872,7384624,12199513,10319645,9158576,1466036,3551766,9752720,8016258,2792641,51384,6236989,2655804,5701105,9032410,2139224,1239590,7058753,10774809,3216079,8717004,11337355,10844965,1693372,6551445,11142080,3791269,11279830,8009338,10498181,7071885,7686988,4954643,8176961,10066962,6898971,10539550,2640120
+SEEDS=523638177,2041280004,1414771290,631534312,1453736814,1397631146,2638535594,1496331968,2726184843,4051158523,3527264775,3958859807,2475161690,948634907,498893635,4011596387,1928773751,2463789121,1355946980,582057396,897038420,677529307,4251148658,1812338824,3650947947,1008507280,3791627088,1505862441,3760189604,4281275324,4256252563,3244565067,2743459145,1003717380,1927117284,3081151930,3321129299,3173403295,4269545983,72630365,2370437039,1961989697,2114798954,2948483590,3035586253,2605854944,3557811144,2428106631,4002200162,276945325
 DOMAIN=vardim_weakhls
 # vardim_icml_nonrealizable
 # vardim_icml_realizable
 # vardim_icml_real_nohls
+
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=leader seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+
+DOMAIN=vardim_icml_nonrealizable
+# vardim_icml_realizable
+# vardim_icml_real_nohls
+
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=leader seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+
+
+DOMAIN=vardim_icml_realizable
+# vardim_icml_real_nohls
+
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=leader seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srlegreedy_mineig_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
+
+DOMAIN=vardim_icml_real_nohls
 
 python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=leader seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
 python new_lbrl_runner.py --multirun horizon=${HORIZON} domain=${DOMAIN}.yaml algo=srllinucb_minfeat_norm seed=${SEEDS} hydra.sweep.dir=new_${DOMAIN}_${TODAY}/\${algo} use_wandb=false use_tb=true eps_decay=cbrt glrt_scale=1 forcedexp=false reg_matrix_rl=false
