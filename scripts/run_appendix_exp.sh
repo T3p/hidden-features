cb16b23c42efb719ba9e7788d8ca4f7893e50a57
Matteo Pirotta <teopir@users.noreply.github.com>
2022-05-20 18:49:38 +0200

    updated

---
+++ scripts/run_appendix_exp.sh
@@ -0,0 +1,29 @@
+HORIZON=400000
+SEEDS=3213,2135,21,5642,9621
+
+########
+# LINUCB
+########
+
+# MSE
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnlinucb check_glrt=True glrt_scale=1,2,4 bonus_scale=0.5,1,2 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=True epsilon_decay=none device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_mse
+
+# min feat
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnlinucb check_glrt=True glrt_scale=1,2,4 bonus_scale=0.5,1,2 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0 weight_min_features=0.5,1,2 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=False epsilon_decay=none device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_minfeat
+
+# rayleigh
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnlinucb check_glrt=True glrt_scale=1,2,4 bonus_scale=0.5,1,2 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0.5,1,2 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=False epsilon_decay=none device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_minfeat
+
+
+########
+# E-GREEDY
+########
+
+# MSE
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnegreedy check_glrt=True glrt_scale=1,2,4 bonus_scale=1 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=True epsilon_decay=cbrt,sqrt device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_mse
+
+# min feat
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnegreedy check_glrt=True glrt_scale=1,2,4 bonus_scale=1 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0 weight_min_features=0.5,1,2 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=False epsilon_decay=cbrt,sqrt device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_minfeat
+
+# rayleigh
+python run_linearfinite.py -m domain=statlog.yaml horizon=${HORIZON} algo=nnegreedy check_glrt=True glrt_scale=1,2,4 bonus_scale=1 layers=\"100,100,50,20,10\",\"100,100\",\"100\" weight_mse=1 weight_rayleigh=0.5,1,2 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=True use_wandb=False use_maxnorm=False epsilon_decay=cbrt,sqrt device="cuda" hydra.sweep.dir=statlog_0520/\${algo}_minfeat
