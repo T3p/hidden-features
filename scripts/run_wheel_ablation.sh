923c89115b196c7b14e6dd77959fb1aa1d9670e8
Ahmed Touati <atouati@fb.com>
2022-06-23 21:02:39 +0200

    squarecb and gradientucb exp

--- scripts/run_wheel_ablation.sh
+++ scripts/run_wheel_ablation.sh
@@ -23,3 +23,19 @@ python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=
 # python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=${ALGO} check_glrt=True glrt_scale=5 layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=1 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=True hydra.sweep.dir=wheel_dataset_0517/\${algo}_trace_maxnorm
 # Trace
 # python run_linearfinite.py -m domain=wheel_dataset.yaml horizon=${HORIZON} algo=${ALGO} check_glrt=True glrt_scale=5 layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=1 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=False hydra.sweep.dir=wheel_dataset_0517/\${algo}_trace
+
+
+
+# squarecb
+ALGO=squarecb
+HORIZON=400000
+SEEDS=713,464,777,879,660,608,773,919,591,229
+python run_linearfinite.py -m domain=wheel.yaml horizon=${HORIZON} algo=${ALGO} gamma_scale=1,10,100 gamma_exponent=sqrt,cbrt check_glrt=False layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=True hydra.sweep.dir=wheel_0622/\${algo}
+
+
+
+# gradientucb
+ALGO=gradientucb
+HORIZON=400000
+SEEDS=713,464,777,879,660,608,773,919,591,229
+python run_linearfinite.py -m domain=wheel.yaml horizon=${HORIZON} algo=${ALGO} bonus_scale=0.1,1,10 check_glrt=False layers=\"100,100,50,20,10\" weight_mse=1 weight_rayleigh=0 weight_min_features=0 weight_min_random=0 weight_l2features=0 weight_trace=0 weight_spectral=0 seed=${SEEDS} use_tb=true use_maxnorm=True hydra.sweep.dir=wheel_0622/\${algo}
--- xbrl/algs/batched/squarecb.py
+++ xbrl/algs/batched/squarecb.py
@@ -17,7 +17,8 @@ class SquareCB(NNLinUCB):
             model: Optional[nn.Module] = None
     ) -> None:
         super().__init__(env, cfg, model)
-        self.gamma = np.sqrt(self.env.action_space.n * np.sqrt(cfg.horizon) + np.log(2. / cfg.delta))
+        self.gamma_exponent = cfg.gamma_exponent
+        self.gamma_scale = cfg.gamma_scale
         self.exploration_param = self.env.action_space.n
         self.time_random_exp = cfg.time_random_exp
         self.np_random = np.random.RandomState(self.seed)
@@ -26,6 +27,10 @@ class SquareCB(NNLinUCB):
         pass
 
     def play_action(self, features: np.ndarray) -> int:
+        if self.gamma_exponent == "cbrt":
+            gamma = self.gamma_scale * np.cbrt(self.t )
+        elif self.gamma_exponent == "sqrt":
+            gamma = self.gamma_scale * np.sqrt(self.t)
         self.is_random_step = 1 # actually it means no glrt action
         features_tensor = torch.tensor(features, dtype=TORCH_FLOAT, device=self.device)
         with torch.no_grad():
@@ -35,12 +40,14 @@ class SquareCB(NNLinUCB):
         opt_arms = np.where(gap <= self.mingap_clip)[0]
         subopt_arms = np.where(gap > self.mingap_clip)[0]
         prob = np.zeros(self.env.action_space.n)
-        prob[subopt_arms] = 1. / (self.exploration_param + self.gamma * gap[subopt_arms])
+        prob[subopt_arms] = 1. / (self.exploration_param + gamma * gap[subopt_arms])
         prob[opt_arms] = (1 - prob[subopt_arms].sum()) / len(opt_arms)
         if self.use_tb:
             self.writer.add_scalar('prob_optarms', 1 - prob[subopt_arms].sum(), self.t)
+            self.writer.add_scalar('gamma', gamma, self.t)
         if self.use_wandb:
-            wandb.log({'prob_optarms':  1 - prob[subopt_arms].sum()}, step=self.t)
+            wandb.log({'prob_optarms':  1 - prob[subopt_arms].sum(),
+                       'gamma': gamma}, step=self.t)
 
         assert np.isclose(prob.sum(), 1)
         action = self.np_random.choice(np.arange(self.env.action_space.n), 1, p=prob).item()
